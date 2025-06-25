e3591d6e810d2abf6750d3b079b035d112fbd58c
ChardonBleu <f.rondi@orange.fr>
2025-06-25 14:16:44 +0200

    💄 style: add mobile style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,28 +6,22 @@ export default function Header() {
   return (
     <>
       <header className="header">
-
         <picture>
           <source media="(max-width: 768px)" srcset={kasaLogo}></source>
           <img src={kasaLogoLarge} alt="logo kasa" className="logo"></img>
         </picture>
 
         <nav className="header__nav">
-          <NavLink 
+          <NavLink
             to="/Kasa/"
-            className={({ isActive }) =>
-              isActive ? "active" : ""
-            }
+            className={({ isActive }) => (isActive ? "active" : "")}
             end
           >
             Accueil
           </NavLink>
           <NavLink
             to="/Kasa/about"
-            className={({ isActive }) =>
-              isActive ? "active" : ""
-            }
-            
+            className={({ isActive }) => (isActive ? "active" : "")}
           >
             A Propos
           </NavLink>
--- src/routes/Error404.jsx
+++ src/routes/Error404.jsx
@@ -3,9 +3,11 @@ import { NavLink } from "react-router";
 export default function Error404() {
   return (
     <>
-      <div>
-        <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
+      <div className="error">
+        <h1 className="error__status">404</h1>
+        <p className="error__message">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
         <NavLink to="/Kasa/">Retourner sur la page d'accueil</NavLink>
       </div>
     </>
